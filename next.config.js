91e636e67128eccb18bcd2bc83a2fda13cfad13e
Sachin Kumar <sachinrajputlive@gmail.com>
2025-05-26 13:22:24 +0530

    [master] initial commit 4

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  output: 'export',
-  basePath: isProd ? '/mcqs-app' : '',
-  assetPrefix: isProd ? '/mcqs-app/' : '',
-};
